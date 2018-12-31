7dd670f053c007442a7129eee7ff1d9cc46a5d52
ponienchen <Wifehoney1>
2018-12-30 21:52:06 -0800

    Initial Project Add

---
+++ src/main/java/peter/springpetclinic/peterpetclinic/PeterPetClinicApplication.java
@@ -0,0 +1,14 @@
+package peter.springpetclinic.peterpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeterPetClinicApplication.class, args);
+    }
+
+}
+
